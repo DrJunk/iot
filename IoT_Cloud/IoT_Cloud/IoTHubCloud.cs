32623763156e9a5b119d72d5ba2678e1c3c8b993
unknown <omer934@walla.co.il>
2017-08-24 12:37:14 +0300

    cloud now working + cool changes

---
+++ IoT_Cloud/IoT_Cloud/IoTHubCloud.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+using Microsoft.ServiceBus.Messaging;
+
+namespace IoT_Cloud
+{
+    class IoTHubCloud
+    {
+        string connectionString;
+        string iotHubD2cEndpoint;
+        ServiceClient serviceClient;
+        EventHubClient eventHubClient;
+
+        public IoTHubCloud()
+        {
+            connectionString = "HostName=MainIoTHub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=f4RkS/lSdFcJcSIoRqaKKKgWnGhlYOe5GSHORqLIZxA=";
+            iotHubD2cEndpoint = "messages/events";
+
+            // for sending messages, connect to iot hub
+            serviceClient = ServiceClient.CreateFromConnectionString(connectionString);
+        
+            // for receiving messages
+            eventHubClient = EventHubClient.CreateFromConnectionString(connectionString, iotHubD2cEndpoint);
+
+        }
+
+        public async Task ListenForMessagesFromDeviceAsync()
+        {
+            string[] d2cPartitions;
+
+            d2cPartitions = eventHubClient.GetRuntimeInformation().PartitionIds;
+            foreach (string partition in d2cPartitions)
+            {
+                var receiver = eventHubClient.GetDefaultConsumerGroup().
+                    CreateReceiver(partition, DateTime.Now);
+                ReceiveMessagesFromDeviceAsync(receiver);
+            }
+        }
+
+        async Task ReceiveMessagesFromDeviceAsync(EventHubReceiver receiver)
+        {
+            while (true)
+            {
+                EventData eventData = await receiver.ReceiveAsync();
+                if (eventData == null) continue;
+
+                string data = Encoding.UTF8.GetString(eventData.GetBytes());
+                Console.WriteLine("Message received: '{0}'", data);
+            }
+        }
+
+        public async Task SendCloudToDeviceMessageAsync(string message)
+        {
+            var commandMessage = new Message(Encoding.ASCII.GetBytes(message));
+            await serviceClient.SendAsync("MainDevice", commandMessage);
+        }
+    }
+}
---
+++ IoT_Device/IoT_Device/IoTHubDevice.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+
+
+class IoTHubDevice
+{
+    //
+    // Note: this connection string is specific to the device "ourDeviceID". To configure other devices,
+    // see information on iothub-explorer at http://aka.ms/iothubgetstartedVSCS
+    //
+    const string deviceConnectionString = "HostName=MainIoTHub.azure-devices.net;DeviceId=MainDevice;SharedAccessKey=c76PJgKGJQ4fkWJxrvexsHQUs08IxR3ufSaWs/dBMDw=";
+    string iotHubUri; 
+    string deviceId;
+    string deviceKey;
+    DeviceClient deviceClient;
+
+    //
+    // To monitor messages sent to device "ourDeviceID" use iothub-explorer as follows:
+    //    iothub-explorer monitor-events --login HostName=NumberAdder.azure-devices.net;SharedAccessKeyName=service;SharedAccessKey=zD23fJNix+kLl2+SEJFvWBb/UQliKuxvslZYCByx6nA= "ourDeviceID"
+    //
+
+    // Refer to http://aka.ms/azure-iot-hub-vs-cs-wiki for more information on Connected Service for Azure IoT Hub
+
+
+    public IoTHubDevice()
+    {
+        //deviceClient = DeviceClient.CreateFromConnectionString(deviceConnectionString, TransportType.Mqtt);
+        iotHubUri = "MainIoTHub.azure-devices.net";
+        deviceId = "MainDevice";
+        deviceKey = "c76PJgKGJQ4fkWJxrvexsHQUs08IxR3ufSaWs/dBMDw=";
+        deviceClient = DeviceClient.Create(iotHubUri,
+        AuthenticationMethodFactory.
+            CreateAuthenticationWithRegistrySymmetricKey(deviceId, deviceKey),
+        TransportType.Http1);
+    }
+
+    public async void SendDeviceToCloudMessageAsync(string message)
+    {
+        var msg = new Message(Encoding.ASCII.GetBytes(message));
+        await deviceClient.SendEventAsync(msg);
+    }
+
+    public async Task<string> ReceiveCloudToDeviceMessageAsync()
+    {
+        //var deviceClient = DeviceClient.CreateFromConnectionString(deviceConnectionString, TransportType.Mqtt);
+
+        while (true)
+        {
+            var receivedMessage = await deviceClient.ReceiveAsync();
+
+            if (receivedMessage != null)
+            {
+                var messageData = Encoding.ASCII.GetString(receivedMessage.GetBytes());
+                await deviceClient.CompleteAsync(receivedMessage);
+                return messageData;
+            }
+
+            await Task.Delay(TimeSpan.FromSeconds(1));
+        }
+    }
+}
