8a8c26e956bb8905756e04f5524770832a5b687e
unknown <omer934@walla.com>
2017-09-10 16:50:41 +0300

    Filled 'Web App' under 'Communication with the cloud'

--- ApiSamples/WeatherHot.py
+++ ApiSamples/WeatherHot.py
@@ -3,24 +3,30 @@ import requests
 import json
 from time import gmtime, strftime, sleep
 
-owm = pyowm.OWM('be353767dba614e5554d0401240408c1')  # You MUST provide a valid API key
+owm = pyowm.OWM('be353767dba614e5554d0401240408c1')  # Connecting to OWM, place a valid free key instead of XXX
 
 location = 'Tel Aviv,isr'
 
-url = 'https://irpihandlemessages.azurewebsites.net/api/HttpGET-HandleApiCalls'
-turnOnData = {'DeviceID' : 'MainDevice',
+url = 'https://irpihandlemessages.azurewebsites.net/api/HttpGET-HandleApiCalls'  	# API's url
+turnOnData = 								# Building the data of the post request
+		{'DeviceID' : 'MainDevice',
           'ProductName' : 'aircon',
-          'ActionName' : 'chmod' }
+          'ActionName' : 'on' }
 		  
 while True:
 	print("Checking weather in " + location + " " + strftime("%Y-%m-%d %H:%M:%S", gmtime()))
+	
+	# Access the temperature using the pyowm lib
 	observation = owm.weather_at_place(location)
 	w = observation.get_weather()
-	temperature = w.get_temperature('celsius')  # {'temp_max': 10.5, 'temp': 9.7, 'temp_min': 9.0}
+	temperature = w.get_temperature('celsius')
+	
 	print (temperature)
+	
 	if(temperature['temp'] < 20):
-		r = requests.post(url, data = json.dumps(turnOnData))
-		print(r.text)
+		r = requests.post(url, data = json.dumps(turnOnData))	# Send a post requset the turns on the air conditioner		
+		print(r.text)						# Print result
 	else:
 		print("Don't turn on aircon")
+		
 	sleep(60)
